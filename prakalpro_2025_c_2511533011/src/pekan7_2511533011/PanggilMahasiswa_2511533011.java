596c0f2fdf188944d02e3cce6188cd9f664243c6
nabilfkrr <fikrin868@gmail.com>
2025-11-11 12:04:47 +0700

    pratikum pekan7_2511533011

---
+++ prakalpro_2025_c_2511533011/src/pekan7_2511533011/PanggilMahasiswa_2511533011.java
@@ -0,0 +1,14 @@
+package pekan7_2511533011;
+
+public class PanggilMahasiswa_2511533011 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533011 a = new Mahasiswa_2511533011();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+	}
+
+}
